2882596efc0b7ef34e65e4b97b3a9554d9e2b77a
JEAN JAE WOONG <johnjin56@gmail.com>
2025-04-04 18:56:30 +0900

    [Gold V] Title: 색칠하기, Time: 488 ms, Memory: 38256 KB -BaekjoonHub

---
+++ 백준/Gold/13265. 색칠하기/색칠하기.py
@@ -0,0 +1,34 @@
+import sys
+sys.setrecursionlimit(10**8)
+input = sys.stdin.readline
+
+def dfs(x, color):
+    global error
+    visited[x] = color
+
+    for i in graph[x]:
+        if visited[i] == 0:
+            dfs(i, -color)
+        elif visited[i] == color:
+            error = True
+            return
+
+K = int(input())
+
+for _ in range(K):
+    V, E = map(int, input().split())
+    
+    graph = [[] for _ in range(V + 1)]
+    visited = [0] * (V + 1)
+    error = False
+    
+    for _ in range(E):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+        
+    for i in range(1, V + 1):
+        if visited[i] == 0:    
+            dfs(i, 1)
+                
+    print("impossible" if error else "possible")
