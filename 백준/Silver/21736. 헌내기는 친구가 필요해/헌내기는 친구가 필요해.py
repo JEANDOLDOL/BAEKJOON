3f2794645fcca525d434f9876335e3576bcfd2df
JEAN JAE WOONG <johnjin56@gmail.com>
2025-06-23 14:47:05 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 668 ms, Memory: 109512 KB -BaekjoonHub

---
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -0,0 +1,37 @@
+import sys
+
+sys.setrecursionlimit(10 ** 8)
+
+N, M = map(int,input().split()) 
+
+visited = [[False] * M for _ in range(N)]
+graph = []
+count = 0
+
+for _ in range(N):
+    a = input()
+    graph.append(a)
+    
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
+
+def dfs(x,y):
+    global count
+    visited[x][y] = True
+    if graph[x][y] == "P":
+        count += 1
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] != "X" and not visited[nx][ny]:
+            dfs(nx,ny)
+            
+for i in range(len(graph)):
+    for e in range(len(graph[0])):
+        if not visited[i][e] and graph[i][e] == "I":
+            dfs(i,e)
+
+if count > 0:
+    print(count)
+else:
+    print("TT")
